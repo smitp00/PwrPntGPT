6e09a4a6d497fe9de021b66ed51be914768fd734
Smit Patel <smitpatel9354@gmail.com>
2023-08-28 21:49:14 -0400

    django framework work with gpt

---
+++ my_project_container/my_app/gpt_service.py
@@ -0,0 +1,26 @@
+# my_app/gpt_service.py
+
+import requests
+import json
+
+def get_summary_from_gpt(content):
+    api_key = "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"
+    
+    headers = {
+        "Authorization": f"Bearer {api_key}",
+        "Content-Type": "application/json"
+    }
+    
+    data = {
+        "prompt": f"Summarize the following text into bullet points:\n{content}",
+        "max_tokens": 100  # You can set the max tokens based on your requirement
+    }
+    
+    response = requests.post("https://api.openai.com/v1/engines/davinci-codex/completions", headers=headers, json=data)
+    
+    if response.status_code == 200:
+        result = json.loads(response.text)
+        summary = result['choices'][0]['text'].strip()
+        return summary
+    else:
+        return "Error in summarization"
--- my_project_container/my_app/views.py
+++ my_project_container/my_app/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from pptx import Presentation
+from .gpt_service import get_summary_from_gpt  # Place gpt_service.py in the my_app directory
 
-# Create your views here.
+def upload_file(request):
+    if request.method == 'POST':
+        file = request.FILES.get('file')
+        if file and file.name.endswith('.pptx'):
+            prs = Presentation(file)
+            slides_text = []
+            for slide in prs.slides:
+                for shape in slide.shapes:
+                    if hasattr(shape, "text"):
+                        slides_text.append(shape.text)
+            summarized_content = get_summary_from_gpt(' '.join(slides_text))
+            return render(request, 'results.html', {'original_content': slides_text, 'summarized_content': summarized_content})
+    return render(request, 'upload.html')
